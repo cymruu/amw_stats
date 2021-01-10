231732b7f2f5dfa67c41f95796295946829c42f8
Filip <fiqxio@gmail.com>
2021-01-10 14:19:56 +0100

    merge plots into one image

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 ./get_data.sh
 (($? != 0)) && { printf '%s\n' "get_data failed set env variable AMW_STATS_MONGO_HOST"; exit 1; }
 mkdir -p plots
+rm plots/*
 Rscript amw_stats.r
+cd plots/
+convert actionsInLastWeek.png activityPerDay.png activityPerHour.png tags.png -append $(date --iso-8601)result.png
