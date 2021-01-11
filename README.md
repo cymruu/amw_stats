e284181013e47368f537617d8dd531283622e0d8
Filip <fiqxio@gmail.com>
2021-01-11 11:02:14 +0100

    updated date command

--- README.md
+++ README.md
@@ -3,4 +3,5 @@ Scripts to generate weekly AMW stats
 
 # usage
 Before using set AMW_STATS_MONGO_HOST env variable to mongodatabase uri. 
+`export AMW_STATS_MONGO_HOST="mongodb://THE_MONGO_URI"`
 Then run ./start.sh
--- get_data.sh
+++ get_data.sh
@@ -1,5 +1,5 @@
 set -u
-SINCE=$(date --date="last Sunday" --iso-8601=seconds)
+SINCE=$(date --date="last Sunday - 7 days" --iso-8601=seconds)
 mongoexport --uri="$AMW_STATS_MONGO_HOST" --collection=confessions --out=confessions.json --query="{\"createdAt\": { \"\$gte\": { \"\$date\": \"$SINCE\" } }}" --jsonArray
 mongoexport --uri="$AMW_STATS_MONGO_HOST" --collection=replies --out=replies.json --query="{\"createdAt\": { \"\$gte\": { \"\$date\": \"$SINCE\" } }}" --jsonArray
 mongoexport --uri="$AMW_STATS_MONGO_HOST" --collection=actions --out=actions.json --query="{\"time\": { \"\$gte\": { \"\$date\": \"$SINCE\" } }}" --jsonArray
