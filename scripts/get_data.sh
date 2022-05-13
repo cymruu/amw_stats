e661c2b3878939b23fa9f1f95d115bad2cccab8d
Filip Bachul <filipbachul@gmail.com>
2022-05-13 23:50:35 +0200

    remove R scripts

--- scripts/get_data.sh
+++
@@ -1,7 +0,0 @@
-# fail if variable is not set
-set -u
-SINCE=$(date --date="last Sunday - 7 days" --iso-8601=seconds)
-mongoexport --uri="$AMW_STATS_MONGO_HOST" --collection=confessions --out=../exported/confessions.json --query="{\"createdAt\": { \"\$gte\": { \"\$date\": \"$SINCE\" } }}" --jsonArray
-mongoexport --uri="$AMW_STATS_MONGO_HOST" --collection=replies --out=../exported/replies.json --query="{\"createdAt\": { \"\$gte\": { \"\$date\": \"$SINCE\" } }}" --jsonArray
-mongoexport --uri="$AMW_STATS_MONGO_HOST" --collection=actions --out=../exported/actions.json --query="{\"time\": { \"\$gte\": { \"\$date\": \"$SINCE\" } }}" --jsonArray
-mongoexport --uri="$AMW_STATS_MONGO_HOST" --collection=users --out=../exported/users.json --fields="_id,username" --jsonArray
--- start.sh
+++
@@ -1,7 +0,0 @@
-./scripts/get_data.sh
-(($? != 0)) && { printf '%s\n' "get_data failed set env variable AMW_STATS_MONGO_HOST"; exit 1; }
-mkdir -p results
-rm results/*
-Rscript amw_stats.r
-cd results/
-convert actionsInLastWeek.png activityPerDay.png activityPerHour.png tags.png -append $(date --iso-8601)result.png
