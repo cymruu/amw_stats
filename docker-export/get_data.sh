04b9f36dda4f6e940566478b5e52c8ed2bc1829b
Filip Bachul <filipbachul@gmail.com>
2022-05-18 00:30:59 +0200

    trigger stats action
    commit 2ae947990c71386cb324fac075bc7ca29e2bdf29
    Author: Filip Bachul <filipbachul@gmail.com>
    Date:   Wed May 18 00:00:39 2022 +0200
    
        test schedule
    
    commit abbe5be9390bab1be61139814d09c0804e1f85b2
    Author: Filip Bachul <filipbachul@gmail.com>
    Date:   Tue May 17 23:45:37 2022 +0200
    
        update readme
    
    commit 8114fbb125fd4db36968d0813a05edc5268f221f
    Author: Filip Bachul <filipbachul@gmail.com>
    Date:   Tue May 17 23:45:29 2022 +0200
    
        disable push trigger
    
    commit 8275710e583e0d791578c0fda10de3c576548b6c
    Author: Filip Bachul <filipbachul@gmail.com>
    Date:   Tue May 17 23:23:35 2022 +0200
    
        add period tag
    
    commit 7cfb168f01327bcd9097fd4b46e9f619b257e657
    Author: Filip Bachul <filipbachul@gmail.com>
    Date:   Tue May 17 23:11:51 2022 +0200
    
        trigger daily
    
    commit 7253f36598988a3f0944c4298940b72453a40e7f
    Author: Filip Bachul <filipbachul@gmail.com>
    Date:   Tue May 17 19:26:17 2022 +0200
    
        daily
    
    commit 3d32fd6756b1072c9bd4f39fe93b28fdb9093e82
    Author: Filip Bachul <filipbachul@gmail.com>
    Date:   Tue May 17 19:13:26 2022 +0200
    
        aa
    
    commit 0631cf532ade3f355a0673f35f2e23578c6180c8
    Author: Filip Bachul <filipbachul@gmail.com>
    Date:   Tue May 17 19:12:44 2022 +0200
    
        test
    
    commit c04d8bb6d2f82b516843152d70c815617199a60e
    Author: Filip Bachul <filipbachul@gmail.com>
    Date:   Tue May 17 00:59:35 2022 +0200
    
        export quietly
    
    commit 89f582af5aef4e1289b49850d8697c8ff900fdfc
    Author: Filip Bachul <filipbachul@gmail.com>
    Date:   Tue May 17 00:57:01 2022 +0200
    
        test
    
    commit 8003b1b6869b77317424f385177fd579ac2a6714
    Author: Filip Bachul <filipbachul@gmail.com>
    Date:   Tue May 17 00:45:23 2022 +0200
    
        rename docker-magisk->docker-imagemagick
    
    commit 919b3154c062de7a85f782368bd8665d3d02b6fe
    Author: Filip Bachul <filipbachul@gmail.com>
    Date:   Tue May 17 00:40:39 2022 +0200
    
        test

--- docker-export/get_data.sh
+++ docker-export/get_data.sh
@@ -4,9 +4,9 @@
 set -e
 
 echo exporting data from: $SINCE - $UNTIL
-mongoexport --uri="$AMW_STATS_MONGO_HOST" --collection=confessions --out=confessions.json --query="{\"createdAt\": {\"\$gte\": {\"\$date\": \"$SINCE\"},\"\$lte\": {\"\$date\": \"$UNTIL\"}}}" --jsonArray
-mongoexport --uri="$AMW_STATS_MONGO_HOST" --collection=replies --out=replies.json --query="{\"createdAt\": {\"\$gte\": {\"\$date\": \"$SINCE\"},\"\$lte\": {\"\$date\": \"$UNTIL\"}}}" --jsonArray
-mongoexport --uri="$AMW_STATS_MONGO_HOST" --collection=actions --out=actions.json --query="{\"time\": { \"\$gte\": { \"\$date\": \"$SINCE\"},\"\$lte\": {\"\$date\": \"$UNTIL\"}}}" --jsonArray
-mongoexport --uri="$AMW_STATS_MONGO_HOST" --collection=users --out=users.json --fields="_id,username" --jsonArray
+mongoexport --quiet --uri="$AMW_STATS_MONGO_HOST" --collection=confessions --out=confessions.json --query="{\"createdAt\": {\"\$gte\": {\"\$date\": \"$SINCE\"},\"\$lte\": {\"\$date\": \"$UNTIL\"}}}" --jsonArray
+mongoexport --quiet --uri="$AMW_STATS_MONGO_HOST" --collection=replies --out=replies.json --query="{\"createdAt\": {\"\$gte\": {\"\$date\": \"$SINCE\"},\"\$lte\": {\"\$date\": \"$UNTIL\"}}}" --jsonArray
+mongoexport --quiet --uri="$AMW_STATS_MONGO_HOST" --collection=actions --out=actions.json --query="{\"time\": { \"\$gte\": { \"\$date\": \"$SINCE\"},\"\$lte\": {\"\$date\": \"$UNTIL\"}}}" --jsonArray
+mongoexport --quiet --uri="$AMW_STATS_MONGO_HOST" --collection=users --out=users.json --fields="_id,username" --jsonArray
 
 mv *.json exported/
--- docker.sh
+++ docker.sh
@@ -2,35 +2,33 @@
 
 # exit if command fails
 set -e
-SINCE=$(date -v-Sun -v-7d -u +"%Y-%m-%dT00:00:00Z")
-UNTIL=$(date -v-Sun -u +"%Y-%m-%dT00:00:00Z")
 
 docker build -t docker-export docker-export
 docker run --rm \
--e AMW_STATS_MONGO_HOST=$AMW_STATS_MONGO_HOST \
--e SINCE=$SINCE \
--e UNTIL=$UNTIL \
+-e AMW_STATS_MONGO_HOST \
+-e SINCE \
+-e UNTIL \
 -v $(pwd)/exported:/workspace/exported \
 docker-export
 
 docker build -t docker-stats docker-stats
-docker run --rm \
--e SINCE=$SINCE \
--e UNTIL=$UNTIL \
+docker run --user root --rm \
+-e SINCE \
+-e UNTIL \
+-e PERIOD \
 -v $(pwd)/exported:/workspace/exported \
--v $(pwd)/pandas:/workspace/pandas \
+-v $(pwd)/results:/workspace/pandas/results \
 docker-stats
 
-
-docker build ./docker-magisk -t docker-magisk
+docker build ./docker-imagemagick -t docker-imagemagick
 docker run --rm \
--v $(pwd)/pandas/results:/results \
-docker-magisk
+-v $(pwd)/results:/results \
+docker-imagemagick
 
-docker build ./docker-post -t docker-post
-docker run --rm \
--v $(pwd)/pandas/results:/results \
--e APPKEY=$APPKEY \
--e SECRET=$SECRET \
--e ACCOUNT_KEY=$ACCOUNT_KEY \
-docker-post
+# docker build ./docker-post -t docker-post
+# docker run --rm \
+# -v $(pwd)/results:/results \
+# -e APPKEY \
+# -e SECRET \
+# -e ACCOUNT_KEY \
+# docker-post
